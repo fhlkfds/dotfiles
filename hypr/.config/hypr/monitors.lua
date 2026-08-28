1fab240b20568dc11f7dc4aafbbd7527a5fc76a7
billy <liamdecareaux2001@gmail.com>
2026-08-28 08:31:14 -0500

    fixing laptops config

--- hypr/.config/hypr/monitors.lua
+++ hypr/.config/hypr/monitors.lua
@@ -1,27 +1,6 @@
 hl.monitor({
     output = "eDP-1",
-    disabled = true,
-})
-
-hl.monitor({
-    output = "DP-5",
-    mode = "1920x1080@60.0",
-    position = "3336x0",
+    mode = "2256x1504@60.0",
+    position = "0x0",
     scale = 1.0,
-})
-
-hl.monitor({
-    output = "DP-7",
-    mode = "1920x1080@60.0",
-    position = "2256x0",
-    scale = 1.0,
-    transform = 1,
-})
-
-hl.monitor({
-    output = "DP-9",
-    mode = "1920x1080@60.0",
-    position = "5256x0",
-    scale = 1.0,
-    transform = 0,
 })
--- hypr/.config/hypr/workspaces.lua
+++ hypr/.config/hypr/workspaces.lua
@@ -1,8 +1,7 @@
 for workspace = 1, 15 do
-    local monitor = workspace <= 5 and "DP-5" or (workspace <= 10 and "DP-7" or "DP-9")
     hl.workspace_rule({
         workspace = tostring(workspace),
-        monitor = monitor,
-        default = workspace == 1 or workspace == 6 or workspace == 11,
+        monitor = "eDP-1",
+        default = workspace == 1,
     })
 end
